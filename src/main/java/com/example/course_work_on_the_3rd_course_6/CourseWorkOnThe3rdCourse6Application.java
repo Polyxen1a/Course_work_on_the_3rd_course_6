563b283a93e092fbfdd0f86a215880e03747ef3e
Polyxen1a <maslowa@inbox.ru>
2023-02-27 18:44:38 +0300

    Initial commit

---
+++ src/main/java/com/example/course_work_on_the_3rd_course_6/CourseWorkOnThe3rdCourse6Application.java
@@ -0,0 +1,13 @@
+package com.example.course_work_on_the_3rd_course_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOnThe3rdCourse6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkOnThe3rdCourse6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_work_on_the_3rd_course_6/CourseWorkOnThe3rdCourse6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_work_on_the_3rd_course_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOnThe3rdCourse6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
